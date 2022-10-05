375bbf4e18f56c0424becc67fef08557c50d13b4
Gabriel Lacerda <gabriel.lacerda1@outlook.com>
2022-10-05 00:39:00 -0300

    Primeiro Commit

---
+++ Atividades Continuas/ac01.py
@@ -0,0 +1,90 @@
+# INSIRA ABAIXO OS NOMES DOS ALUNOS DO GRUPO (máximo 6 alunos)
+# ALUNO 1: Gabriel Lacerda Abilio
+# ALUNO 2: Gildasio Santos Xavier Filho
+# ALUNO 3: Igor dos Santos Ferreira
+# ALUNO 4: Diogo Fernando da Costa Mota
+# ALUNO 5: Victor Hugo Silva Nunes
+# ALUNO 6: Guilherme Lima dos Santos
+
+
+'''
+Implemente a função 'posicoes' que recebe como argumentos de entrada uma
+tupla e um item, e retorna uma lista contendo todos os índices em que o
+item aparece na tupla.
+Caso o item nao exista na tupla, deve retornar uma lista vazia.
+'''
+
+
+def posicoes(tupla, item):
+    indices = []
+    for i in range(len(tupla)):
+        if (tupla[i] == item):
+            indices.append(i)
+    return indices
+
+
+'''
+Implemente a função 'remover_itens_repetidos' que recebe como argumento
+de entrada uma lista e remove todos os itens repetidos dessa lista. A função
+deve retornar uma nova lista sem itens repetidos e ordenada de forma crescente.
+'''
+
+
+def remover_itens_repetidos(lista):
+    newLista = list(set(lista))
+    return newLista
+
+
+'''
+Considere um dicionario onde a chave é o nome de um aluno e o valor uma lista
+de notas. Implemente a função 'media_notas' que recebe como argumento de
+entrada o dicionário e retorna outro dicionário contendo os nomes e as médias
+aritméticas de cada aluno.
+'''
+
+
+def media_notas(alunos):
+    medias = {}
+    for aluno in alunos:
+        medias[aluno] = sum(alunos[aluno]) / len(alunos[aluno])
+    return medias
+
+
+'''
+Considere um dicionário onde a chave é o nome de um aluno e o valor uma lista
+de notas. Implemente a função 'excluir_menor_nota' que recebe como argumentos
+de entrada o dicionário e o nome de um aluno. A função deve excluir a menor
+nota do aluno informado e retornar o dicionário com as alterações realizadas.
+Se aluno não estiver no dicionário, deve retornar o dicionário sem alterações.
+'''
+
+
+def excluir_menor_nota(alunos, nome):
+    if (nome in alunos):
+        menor = alunos[nome][0]
+        for i in alunos[nome]:
+            if (menor > i):
+                menor = i
+        alunos[nome].remove(menor)
+    return alunos
+
+
+'''
+Implemente a função 'caracter_mais_frequente' que recebe como argumento de
+entrada uma string e retorna o caracter que aparece com mais frequência na
+string. Procure utilizar um dicionário para facilitar a implementação.
+'''
+
+
+def caracter_mais_frequente(texto):
+    caracter = {}
+    chrMaisFrequente = ["a", 0]
+    for chr in texto:
+        if (chr in caracter):
+            caracter[chr] += 1
+        else:
+            caracter[chr] = 1
+    for letra in caracter:
+        if (caracter[letra] > chrMaisFrequente[1]):
+            chrMaisFrequente = [letra, caracter[letra]]
+    return chrMaisFrequente[0]
---
+++ Atividades Continuas/ac02.py
@@ -0,0 +1,82 @@
+'''''
+-----------
+DESCRIÇÃO 
+-----------
+João confecciona placas por encomenda. Como o volume dos pedidos tem aumentado, ele precisa de uma 
+aplicação que controle o cadastro de seus clientes e os pedidos realizados.
+Quando ele recebe uma encomenda, João anota o nome do cliente, seu endereço completo e seu telefone.
+Para a encomenda, ele registra: a altura e largura da placa, a frase a ser escrita, a cor da placa
+("branca" ou "cinza"), a cor da frase ("azul", "vermelha", "amarela", "preta" ou "verde"). 
+Com base nessas informações ele calcula manualmente o valor da placa, utilizando as seguintes fórmulas:
+área = altura x largura
+custo do material = área x R$ 147,00
+custo do desenho = quantidade de letras x R$ 0,35 (espaços devem ser desconsiderados)
+valor final da placa = custo do material + custo do desenho
+
+João deseja que o sistema controle os pedidos e calcule automaticamente o valor final das placas.
+João deseja ainda manter um histórico de todos os pedidos realizados, a fim de verificar o seu faturamento.
+A partir do cenário descrito e do diagrama de classes apresentado, implemente em Python
+as classes solicitadas, aplicando os conceitos de programação orientada a objetos.
+'''
+class Endereco:
+    def __init__(self, rua, numero, complemento, bairro, cidade, uf, cep):
+        self.rua = rua
+        self.numero = numero
+        self.complemento = complemento
+        self.bairro = bairro
+        self.cidade = cidade
+        self.uf = uf
+        self.cep = cep
+
+
+class Cliente:
+    def __init__(self, nome, telefone, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.endereco = endereco
+
+
+class Pedido:
+    def __init__(self, cliente, altura, largura, frase, cor_placa, cor_letra):
+        self.cliente = cliente
+        self.altura = altura
+        self.largura = largura
+        self.frase = frase
+        self.cor_placa = cor_placa
+        self.cor_letra = cor_letra
+        self.valor_fixo_material = 147.00
+        self.valor_fixo_letra = 0.35
+
+    def calcular_total(self):
+        letras = len(self.frase.replace(" ", ""))
+        area = self.altura * self.largura
+        custoMaterial = area * self.valor_fixo_material
+        custoDesenho = letras * self.valor_fixo_letra
+        total = custoMaterial + custoDesenho
+        return total
+
+
+endereco = Endereco("Avenida Brasil", "123", "Apto. 58", "Centro", "São Paulo",
+                    "SP", "05577-023")
+
+cliente = Cliente("Paulo", "(11) 99999-4565", endereco)
+
+pedido1 = Pedido(cliente, 1.0, 3.0, "10 vezes sem juros", "cinza", "vermelha")
+
+pedido2 = Pedido(cliente, 0.5, 2, "Estamos Atendendo", "cinza", "vermelha")
+
+pedido3 = Pedido(cliente, 2, 5, "Promoção de inauguração", "branca", "preta")
+
+
+class Historico:
+    def __init__(self):
+        self.pedidos = []
+
+    def inserir_pedido(self, pedido):
+        self.pedidos.append(pedido)
+
+    def calcular_faturamento(self):
+        total = []
+        for pedido in self.pedidos:
+            total.append(pedido.calcular_total())
+        return sum(total)
